59c4fe69f94c1b50ce4caa71519f014b2bc1170c
David Brazdil <dbrazdil@google.com>
2014-06-23 14:51:31 -0700

    Moved folder configuration into a separate file

--- link_nonsfi_prog.sh
+++ link_nonsfi_prog.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+source 'config.inc'
+
 set -eu
 
 if [ "$#" != 2 ]; then
   echo Usage: $0 input.o output_prog
   exit 1
 fi
-
-nacl_dir=~/devel/nacl-git3/native_client
-tc_bin=$nacl_dir/toolchain/pnacl_linux_x86/host_x86_64/bin
 
 input="$1"
 output="$2"
@@ -27,7 +26,7 @@ pnacl-opt \
     -expand-allocas -allocate-data-segment \
     -sandbox-memory-accesses -sandbox-indirect-calls \
     $input -o $output.bc
-$tc_bin/llc -mtriple=x86_64-linux-gnu -relocation-model=pic -filetype=obj \
+pnacl-llc -mtriple=x86_64-linux-gnu -relocation-model=pic -filetype=obj \
     $output.bc -o $output.o
 objcopy --redefine-sym _start=sandbox_entry $output.o $output.o
 gcc -g -m64 -Wall -Werror trusted_runtime.c $output.o -o $output
--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+source 'config.inc'
+
 set -eux
 
-nacl_dir=~/devel/nacl-git3/native_client
-chromium_dir=~/devel/newgit-chromium/src
-zlib_dir=$chromium_dir/third_party/zlib
+zlib_dir=$DIR_CHROMIUM/third_party/zlib
 
 rm -rf out
 mkdir -p out
@@ -13,7 +13,7 @@ mkdir -p out
 # Build and run minsfi_test
 
 cflags="-Wall -Werror"
-pnacl-clang $cflags -c $nacl_dir/src/untrusted/nacl/lock.c -o out/lock.c.o
+pnacl-clang $cflags -c $DIR_NACL/src/untrusted/nacl/lock.c -o out/lock.c.o
 pnacl-clang $cflags -c untrusted_support.c -o out/untrusted_support.c.o
 pnacl-clang $cflags -c minsfi_test.c -o out/minsfi_test.c.o
 pnacl-ld -r out/lock.c.o out/untrusted_support.c.o out/minsfi_test.c.o -lc \
@@ -32,10 +32,10 @@ for f in inflate.c inftrees.c inffast.c adler32.c crc32.c zutil.c \
          ; do
   files="$files $zlib_dir/$f"
 done
-files="$files $nacl_dir/src/untrusted/nacl/lock.c untrusted_support.c"
+files="$files $DIR_NACL/src/untrusted/nacl/lock.c untrusted_support.c"
 
 # USE_FILE32API tells ioapi.c not to use fopen64().
-cflags="-g -Wall -I$chromium_dir -I$nacl_dir -DUSE_FILE32API"
+cflags="-g -Wall -I$DIR_CHROMIUM -I$DIR_NACL -DUSE_FILE32API"
 obj_files=""
 for file in $files; do
   obj="out/$(basename $file).o"
